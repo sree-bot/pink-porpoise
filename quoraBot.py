7ae379adafb8a99164348a1e59b96c10567452b6
Sreekanth Ramakrishnan <sree-bot@fake.server>
2013-04-08 13:32:08 +0530

    Added Persistance into local sqlite store.

--- quoraBot.py
+++ quoraBot.py
@@ -2,9 +2,36 @@ import praw
 import quoraCrawler
 import AccountDetails
 import time
+import sqlite3 as lite
+
+def initalize_db():
+    con = lite.connect('submissions.db')
+    with con:
+        cur = con.cursor()
+        cur.execute('create table if not exists submissions(submission text)')
+        
+
+def readInitalValues():
+    already_done = []
+    con = lite.connect('submissions.db')
+    with con:
+        cur = con.cursor()
+        cur.execute('select submission from submissions')
+        rows = cur.fetchall()
+        for row in rows:
+            already_done.append(row[0])
+    return already_done
+
+def persistSubmission(id):
+    con = lite.connect('submissions.db')
+    with con:
+        cur = con.cursor()
+        cur.execute('INSERT INTO submissions(submission) values ("'+id+'")')
 
 if __name__ == "__main__":
-    already_done  = []
+    initalize_db()
+    already_done  = readInitalValues()
+    print "Already done submission are " + ",".join(already_done)
     #Initaize the crawler
     quoraCrawler.initalize()
     r = praw.Reddit(user_agent = "quorabot")
@@ -18,6 +45,7 @@ if __name__ == "__main__":
                 finalcomment = quoraCrawler.getFinalComments(submission.url)
                 submission.add_comment(finalcomment)
                 already_done.append(submission.id)
+                persistSubmission(submission.id)
                 print "Submitted to : " + submission.id            
                 # sleep so that quora does not ban us
                 time.sleep(10)
